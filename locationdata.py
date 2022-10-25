9056b1f4b4c46f24cf22b2f9636576df57f0ca27
Nicole Zheng <70242621+nicholszheng@users.noreply.github.com>
2022-10-24 21:30:44 -0400

    Update locationdata.py
    created variable to combine location data. created print function

--- locationdata.py
+++ locationdata.py
@@ -1,6 +1,10 @@
-country = "USA"
-state = "Florida"
-city = "Miami"
+country = "USA "
+state = "Florida "
+city = "Miami "
 continent = "North America"
 
 # now the travel journal is up to date!
+
+location_data = state + city + country + continent
+
+print("My location data is " + location_data)
