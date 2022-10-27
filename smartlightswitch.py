8d7858b5c8b0deb53abdc74e6e2adfdc1708d4ba
Nicole Zheng <70242621+nicholszheng@users.noreply.github.com>
2022-10-27 18:38:13 -0400

    commit smart light switch

---
+++ smartlightswitch.py
@@ -0,0 +1,10 @@
+# this will show how to switch lights off when its daytime and on when its nighttime.
+
+is_day = False
+lights_on = not is_day
+
+print("Daytime?")
+print (is_day)
+
+print("Lights on?")
+print(lights_on)
